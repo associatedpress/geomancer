9c67697a4c7670c48eedf9774a388ec53b0cd299
Cathy Deng <cathyd125@gmail.com>
2014-09-10 10:34:42 -0500

    fix csv reading bug - only try to convert non-csv files to csv

--- geo/views.py
+++ geo/views.py
@@ -25,47 +25,52 @@ def select_geo():
     f = request.files['input_file']
     inp = StringIO(f.read())
     file_format = convert.guess_format(f.filename)
-    try:
-        converted = convert.convert(inp, file_format)
-    except UnicodeDecodeError:
-        context['errors'] = ['We had a problem with reading your file. \
-            This could have to do with the file encoding or format']
-        converted = None
-    f.seek(0)
-    if len(f.next()) == len(converted):
-        f.seek(0)
-        reader = UnicodeCSVReader(f)
-        checker = RowChecker(reader)
-        for row in checker.checked_rows():
-            pass
-        if checker.errors:
+    if file_format != 'csv':
+        try:
+            converted = convert.convert(inp, file_format)
+        except UnicodeDecodeError:
+            context['errors'] = ['We had a problem with reading your file. \
+                This could have to do with the file encoding or format']
             converted = None
-            context['errors'] = ['We had a problem processing your file']
-            context['errors'].extend(['Line %s: %s' % (l + 1, v) \
-                for l,v in enumerate(checker.errors[:10])])
-    if converted:
-        outp = StringIO(converted)
-        reader = UnicodeCSVReader(outp)
-        context['header_row'] = reader.next()
-        rows = []
-        columns = [[] for c in context['header_row']]
-        column_ids = range(len(context['header_row']))
-        for row in range(10):
-            try:
-                rows.append(reader.next())
-            except StopIteration:
-                break
-        for i, row in enumerate(rows):
-            for j,d in enumerate(row):
-                columns[j].append(row[column_ids[j]])
-        columns = [', '.join(c) for c in columns]
-        sample_data = []
-        for index,_ in enumerate(context['header_row']):
-            sample_data.append((index, context['header_row'][index], columns[index]))
-        context['sample_data'] = sample_data
-        outp.seek(0)
-        session['file'] = outp.getvalue()
-        session['filename'] = f.filename
+        f.seek(0)
+        if len(f.next()) == len(converted):
+            f.seek(0)
+            reader = UnicodeCSVReader(f)
+            checker = RowChecker(reader)
+            for row in checker.checked_rows():
+                pass
+            if checker.errors:
+                converted = None
+                context['errors'] = ['We had a problem processing your file']
+                context['errors'].extend(['Line %s: %s' % (l + 1, v) \
+                    for l,v in enumerate(checker.errors[:10])])
+        if converted:
+            outp = StringIO(converted)
+    else:
+        inp.seek(0)
+        outp = inp
+
+    reader = UnicodeCSVReader(outp)
+    context['header_row'] = reader.next()
+    rows = []
+    columns = [[] for c in context['header_row']]
+    column_ids = range(len(context['header_row']))
+    for row in range(10):
+        try:
+            rows.append(reader.next())
+        except StopIteration:
+            break
+    for i, row in enumerate(rows):
+        for j,d in enumerate(row):
+            columns[j].append(row[column_ids[j]])
+    columns = [', '.join(c) for c in columns]
+    sample_data = []
+    for index,_ in enumerate(context['header_row']):
+        sample_data.append((index, context['header_row'][index], columns[index]))
+    context['sample_data'] = sample_data
+    outp.seek(0)
+    session['file'] = outp.getvalue()
+    session['filename'] = f.filename
     return render_template('select_geo.html', **context)
     
 
