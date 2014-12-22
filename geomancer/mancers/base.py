d86b2fa953a7dfb507f0dc540b6b2d280e695d89
Cathy Deng <cathyd125@gmail.com>
2014-12-22 11:21:47 -0600

    comments

--- geomancer/mancers/base.py
+++ geomancer/mancers/base.py
@@ -75,10 +75,10 @@ class BaseMancer(scrapelib.Scraper):
             },
             {
               'table_id':    # table id for api lookups, 
-              'human_name':  # this shows up as a table row on the /select-tables page,
+              'human_name':  # this shows up as a table row on the /select-tables page, the /data-sources page, & the /geographies page,
               'description': '<free form text description>',
-              'source_name': '<name of data source>',
-              'source_url':  '<where to find source on the web>',
+              'source_name': # this shows up as link text under 'Source' on the /geographies page,
+              'source_url':  # this is the link url under 'Source' on the /geographies page,
               'geo_types':   # this determines the geographies that can be matched,
               'count':       # this shows up under 'Columns that will be added' on the /select-tables page,
               'columns':     # each list item shows up in the popup when clicking the column info link on the /select-tables page
