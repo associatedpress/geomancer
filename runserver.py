7c33ff3ff94b933fe9e5c8b53fa08041ef8ee404
Eric van Zanten <eric.vanzanten@gmail.com>
2014-12-22 14:28:16 -0600

    Make it so we can choose port

--- runserver.py
+++ runserver.py
@@ -3,4 +3,9 @@ from geomancer import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import sys
+    try:
+        port = int(sys.argv[1])
+    except IndexError:
+        port = 5000
+    app.run(debug=True, port=port)
